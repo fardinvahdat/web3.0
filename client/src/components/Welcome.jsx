2eb5287d1292d15546e32a972fa0506fa816f50c
fardin <vahdatfardin@gmail.com>
2023-09-11 18:22:26 +0330

    add custom tailwind config and use it

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,8 +23,8 @@ const Welcome = () => {
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-white text-3xl sm:text-5xl text-gradient py-1">
             Send Crypto <br />
             accross world
